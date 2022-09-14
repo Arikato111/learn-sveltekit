5dbf5dc84bb41dabfa95613e92c0b7c5f26fe1f6
nawasan <arikato110011@gmail.com>
2022-09-14 09:17:13 +0700

    initial commit

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+    <title>Contact</title>
+    <meta name="description" content="just for test 555">
+</svelte:head>
+
+<div>
+    <h1>Just Contact page</h1>
+    <hr>
+    <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi sequi quae reiciendis dolorem necessitatibus, officiis officia qui quod similique distinctio sint magni excepturi repudiandae totam nostrum esse quos exercitationem voluptatibus.
+        Ad, qui iste cumque exercitationem laudantium vitae quisquam laborum culpa praesentium molestias dolorem odit obcaecati sit aspernatur dolore hic id dignissimos? Esse provident ipsa quo quidem animi eos, nobis neque!
+        Amet facilis exercitationem nam soluta vero distinctio cumque earum nesciunt recusandae, quis delectus, provident aut quae quo iusto iure laudantium atque. Nesciunt consequuntur commodi omnis provident quidem esse sint atque.
+        Quis veniam pariatur ducimus laborum harum dolorum voluptatibus magnam. Quis possimus ipsam aliquam quod quaerat facere at doloremque corrupti quas doloribus aliquid quia minus beatae, alias dolore impedit mollitia aperiam.
+        Id hic quis aspernatur optio repellat necessitatibus suscipit at tempora recusandae nostrum exercitationem totam tempore eos rem consequatur quibusdam possimus provident atque esse doloribus nulla, nisi voluptate? Amet, expedita officia?
+        Aliquid rerum illo sapiente dolorum corporis, blanditiis odio quas minus fuga suscipit tempore quia inventore expedita dignissimos, et beatae delectus saepe sint, ducimus praesentium? Eum ipsum optio doloremque soluta sapiente!
+        Mollitia autem blanditiis necessitatibus, fugit aliquid iure sapiente asperiores quaerat aperiam in temporibus nobis cupiditate enim magnam dolores culpa reprehenderit quisquam porro delectus itaque nemo possimus similique? Optio, eum neque.
+        Quam recusandae iusto adipisci cupiditate eligendi! Exercitationem pariatur est illo nisi reprehenderit voluptates, temporibus eveniet assumenda fuga natus quas explicabo itaque ipsum porro ullam vitae non facere ducimus rem aut.
+        Quo, repellat nihil id corporis similique sit magnam pariatur quibusdam ducimus maiores exercitationem vero obcaecati nisi modi non a velit vel cupiditate dolores impedit accusantium! Officia animi accusantium voluptates iure?
+        Recusandae quo iste consequuntur, magni tenetur assumenda facilis illo iure nam. Doloribus fugit quis nisi voluptatem quisquam excepturi neque ab officia laboriosam architecto ipsum, veritatis sequi dolor pariatur! Pariatur, quisquam!
+    </div>
+</div>
