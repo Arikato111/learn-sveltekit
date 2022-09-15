ee81faf99ada700952c08bcd2744c374f25e4ff7
nawasan <arikato110011@gmail.com>
2022-09-15 12:28:57 +0700

    add new func

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -30,6 +30,9 @@
 			<li class:active={$page.url.pathname == '/testtodo'}>
 				<a href="/testtodo">Test Todo</a>
 			</li>
+			<li class:active={$page.url.pathname == '/input-code'}>
+				<a href="/input-code">Input Code</a>
+			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
---
+++ src/routes/input-code/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+  let code = "@ <br> <div align=\"left\">hello world</div>";
+  addEventListener("keydown", function (e) {
+    if (e.ctrlKey && e.key == "k") {
+      /* Do whatever */
+      console.log(e);
+      // @ts-ignore
+      window.document.getElementById("input").focus();
+      e.preventDefault();
+    }
+  });
+</script>
+
+<div>
+  <textarea
+    id="input"
+    placeholder="Ctrl + k to focus"
+    class="w-full p-2 focus:outline-none rounded"
+    rows="10"
+    bind:value={code}
+  ></textarea>
+  <div class="p-3 my-3 rounded bg-white">
+    {#if code[0] && code[0] ==='@'}
+    <span>{@html code}</span>
+    {:else}
+    {#each code as c}
+    <span>{c}</span>
+    {/each}
+    {/if}
+  </div>
+</div>
